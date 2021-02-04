34fa29979deabb1cdd6b35c3e690d10f812b8465
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-02-04 17:17:21 +0200

    Redis sentinel with TLS fully completed.

--- docker/generate-certificates.sh
+++ docker/generate-certificates.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 mkdir -p certs
+
+# Generate RSA private key 'ca.key'
 openssl genrsa -out certs/ca.key 4096
+
+# Generate certificate with public key 'ca.crt'
 openssl req \
     -x509 -new -nodes -sha256 \
     -key certs/ca.key \
     -days 3650 \
     -subj '/O=Oracle/CN=Certificate Authority' \
     -out certs/ca.crt
+
+# Generate RSA private key 'redis.key'
 openssl genrsa -out certs/redis.key 2048
+
+# Generate 'redis.crt' certificate using 'ca.key' as CA signer key
+# so we have the following certificates chain: redis --> ca
 openssl req \
     -new -sha256 \
     -key certs/redis.key \
@@ -20,4 +29,6 @@ openssl req \
         -CAcreateserial \
         -days 365 \
         -out certs/redis.crt
+
+# Generate Diffie-Hellman
 openssl dhparam -out certs/redis.dh 2048
