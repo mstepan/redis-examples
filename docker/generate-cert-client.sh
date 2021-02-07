c8acc5529eceb4d0c2ca2e3648713db9b80a111a
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-02-07 15:57:31 +0200

    few shell scripts added to view keytool & openssl certificates

--- docker/generate-cert-client.sh
+++ docker/generate-cert-client.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+store_password=611191
+echo "Truststore/keystore password: "  $store_password
+
 #------------------------------------------------
 # Client certificate
 #------------------------------------------------
@@ -39,16 +42,20 @@ rm -rf $CERTS_CLIENT_FOLDER/keystore.jks
 
 # Convert to pkcs12 format
 openssl pkcs12 -export \
+  -password pass:$store_password \
   -in $CERTS_CLIENT_FOLDER/client.pem \
   -inkey $CERTS_CLIENT_FOLDER/client.key > $CERTS_CLIENT_FOLDER/client.p12
 
 # Import client certificate to JKS keystore
 keytool -importkeystore -srckeystore $CERTS_CLIENT_FOLDER/client.p12 \
   -destkeystore $CERTS_CLIENT_FOLDER/keystore.jks \
-  -srcstoretype pkcs12
+  -srcstoretype pkcs12 \
+  -storepass $store_password
 
 # Add 'redis.pem' or 'ca.pem' certificate to JKS truststore
-keytool -import -v -trustcacerts -alias ca -file $CERTS_FOLDER/ca.pem -keystore $CERTS_CLIENT_FOLDER/truststore.jks
+keytool -import -v -trustcacerts -alias ca -file $CERTS_FOLDER/redis.pem \
+  -keystore $CERTS_CLIENT_FOLDER/truststore.jks \
+  -storepass $store_password
 
 # List JKS keystore entries
 #keytool -list -v -keystore $CERTS_CLIENT_FOLDER/keystore.jks
