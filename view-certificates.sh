23f9b1fd9b419e870064d834cf61d0e382f20cb4
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-02-07 11:24:22 +0200

    view-certificate shell added to view certificates content using openssl command

--- view-certificates.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-openssl x509 -in docker/certs/ca.pem -text -noout
-openssl x509 -in docker/certs/redis.pem -text -noout
-openssl x509 -in docker/certs-client/client.pem -text -noout
